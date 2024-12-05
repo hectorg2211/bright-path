ae47028b263c8936646233ed8bac8ba580e1ee28
Hector Garcia <hector032716@gmail.com>
2024-12-04 19:26:55 -0800

    add navbar

---
+++ app/(dashboard)/_components/mobile-sidebar.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Menu } from 'lucide-react'
+
+const MobileSidebar = () => {
+  return <div>MobileSidebar</div>
+}
+
+export default MobileSidebar
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Siderbar from './_components/sidebar'
+import Navbar from './_components/navbar'
 
 interface DashboardLayoutProps {
   children: React.ReactNode
@@ -8,10 +9,15 @@ interface DashboardLayoutProps {
 const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) => {
   return (
     <div className='h-full'>
+      <div className='h-[80px] md:pl-56 fixed inset-y-0 wfull min-w-full z-50'>
+        <Navbar />
+      </div>
+
       <div className='hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50'>
         <Siderbar />
       </div>
-      {children}
+
+      <main className='md:pl-56 h-full'>{children}</main>
     </div>
   )
 }
